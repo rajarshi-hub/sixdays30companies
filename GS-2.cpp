83656378ea10be7e33840f7f1159497486f92bc4
Rajarshi Tiwari <62015785+rajarshi-hub@users.noreply.github.com>
2022-01-01 13:49:24 +0530

    GS-2

--- GS-2.cpp
+++ GS-2.cpp
@@ -13,7 +13,7 @@ class Solution {
         return 0;
         if(!((y1 >= y3 && y2 <= y3) || (y1 >= y4 && y2 <= y4) || (y3 >= y1 && y1 >= y4) || (y3 >= y2 && y2 >= y4)))
         return 0;
-        return 1;
+        return 1; //Found the overlap Condition
         
         
         
