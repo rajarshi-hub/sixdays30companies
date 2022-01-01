43054c346bf26e64413edc8540354b4fb384c9a9
Rajarshi Tiwari <62015785+rajarshi-hub@users.noreply.github.com>
2022-01-01 14:31:14 +0530

    Create GS-4.cpp

---
+++ GS-4.cpp
@@ -0,0 +1,18 @@
+string encode(string src)
+{     
+  string ans = "";
+  for(int i=0;i<src.size();)
+  {
+      char ch = src[i];
+      int c = 0;
+      while(i < src.size() && src[i] == ch)
+      {
+          c++;
+          i++;
+      }
+      ans+=ch;
+      ans+=to_string(c);
+  }
+  return ans;
+}     
+ 
